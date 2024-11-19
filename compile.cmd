abf645fe8fb808593dd3f7274650d0552c0d69ea
Федор Лукашов <fiodorlukashov@gmail.com>
2024-11-18 18:15:34 -0800

    Change compile script

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,2 @@
-cmd /k "poetry install --no-root & .venv\Scripts\activate & python compile.py"
+if not exist .venv (python -m venv .venv)
+cmd /k "git pull & pip install poetry & poetry install --no-root & .venv\Scripts\activate & python compile.py"
